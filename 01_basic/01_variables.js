e8a4355a778249a6f0482933dc7e84ba9b2aa859
gitavhi <devkotaavishek208@gmail.com>
2024-07-08 10:10:12 +0000

    done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,17 @@
+const account_ID = 155443 
+let accountEmail = "huhuhu@gmail.com"
+var accountPassowrd = "1233"
+accountCity = "Jaipur"
+let accountState;
+//account_ID = 2// not allowed
+accountEmail = "khkhkh@gmail.com"
+accountPassowrd - "2222"
+accountCity = "KTM"
+console.log(accountEmail);
+
+/*
+PREFER NOT TO USE VAR
+BECAUSE OF ISSUUE IN BLOCK SCOPE AND FUNCTIONAL SCOPE
+
+*/
+console.table([account_ID,accountEmail,accountCity,accountState])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,3 @@
+"use strict"; // treat all JS code as newer verison
+
+//alert (3+3)//we are using node js not browser 
