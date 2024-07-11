1c72b41766699232933c6384bea62620780c304c
gitavhi <devkotaavishek208@gmail.com>
2024-07-11 06:30:07 +0000

    conversion of data types

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,5 +1,23 @@
- let score = "66"
- console.log(typeof(score));
+ //let score = "66aa"
+ //console.log(typeof(score));
 
- let valueInNumber = Number(score)
- console.log(typeof(valueInNumber));
+// let valueInNumber = Number(score)
+ //console.log(typeof(valueInNumber));
+ //console.log(valueInNumber);
+
+
+ // "33"=> 33
+ // "33ABC" => NaN
+ //  true =>1; false=> 0
+     
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+/*
+1 = true 0 = false
+"" = false 
+"hitesh"= true
+*/
