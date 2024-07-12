5cf3182c73e58418b84d3cf3cb0f1ade74eebe03
gitavhi <devkotaavishek208@gmail.com>
2024-07-12 07:49:09 +0000

    ooo

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,30 @@
+/*Two types of data types
+1.premitive
+2.non premetive
+(divided in the basis of how they
+store data and acess the stored data)
+
+- PREMITIVE ( 7 types)
+string,number,boolearn,null,undefined,
+symbol(used to make any values unique),
+BigInt
+*/
+const score = 100 //number
+const useraIsLoggedIn = true //boolearn
+const outsideTemp = null //null
+let userEmail ; //undefined
+
+/*
+
+- REFERNCE TYPE(NON PRIMITIVE)
+array,objects,functions.
+
+*/
+const heros = ["shaktiman","nagraj","rajeshhamal"]
+// array
+
+let my obj = {
+    name: avi
+    age: 00
+}// object
+
