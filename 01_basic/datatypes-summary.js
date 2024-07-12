96ff98f95bc5e3166ce6f23a19e3b272a50ddbe2
gitavhi <devkotaavishek208@gmail.com>
2024-07-12 16:02:10 +0000

    change in locatiion

--- 01_basic/datatypes-summary.js
+++ 01_basic/datatypes-summary.js
@@ -23,8 +23,35 @@ array,objects,functions.
 const heros = ["shaktiman","nagraj","rajeshhamal"]
 // array
 
-let my obj = {
-    name: avi
-    age: 00
-}// object
+ let myObj = {
+    name:"avi",
+    age:22,
+} 
+//object
 
+const myFunction = function (){
+    console.log("hello world");
+}
+/*
+######## finding the data types using (typeof) 
+______________________________________________________________________________________________________
+Type of val	                                              Result  
+_____________________________________________________________________________________________________
+1 - Undefined	---------------------------------------> "undefined"
+2 - Null	-------------------------------------------> "object"
+3 - Boolean -------------------------------------------->  "boolean"
+4 - Number --------------------------------------------->   "number"
+5 - String	------------------------------------------->   "string"
+6 - Object (native and does 
+   not implement [[Call]])	--------------------------->   "object"
+7 - Object (native or host and 
+ does implement [[Call]])	---------------------------> "function"
+8 - Object (host and does not implement [[Call]]) ------>  Implementation-defined except may not be 
+                                                           "undefined", "boolean", "number", or "string".
+*/  
+_____________________________________________________________________________________________________________
+ 
+// using above function to find data type
+let number = 1000
+console.log( number );
+console.log(typeof number); 
