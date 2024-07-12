610e49908708bbb0c6150f8b1018f299dc4f6f39
gitavhi <devkotaavishek208@gmail.com>
2024-07-12 07:00:41 +0000

    comparasion done

---
+++ 01_basic/04_comparasion.js
@@ -0,0 +1,28 @@
+/*
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+( this types of conv is normal conversionn)
+      data types is samee !!!!!!!!!!!!
+*/
+
+
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+(problem beacuse it conver it into null and nan and
+ we should avoid this types of comparision)
+  
+
+
+console.log(undefined == 0); 
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+(result = false )
