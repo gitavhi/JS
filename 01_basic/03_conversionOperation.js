f9e27d2acc6ca6c0f3e4742ff9d3f2ad1780a8f9
gitavhi <devkotaavishek208@gmail.com>
2024-07-12 06:16:30 +0000

    convoo

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -27,3 +27,24 @@
 
 // ************** Operations **************//
  
+let value = 2
+let negValue = -value
+//console.log(negValue);
+
+//console.log("1" + 2);=12
+//console.log(1 + "2");=12
+//console.log("1" + 2 + 2);=122
+//console.log(1 + 1 + "2");=22
+
+let num1,num2,num3
+num1 = num2 = num3 = 4
+//console.log(num1);
+
+
+ let gameCounter = 100
+ ++ gameCounter;
+ //console.log(gameCounter);
+
+ let x = 3;
+const y = x ++; 
+console.log(y);
