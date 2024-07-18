3868ca24378a155db168219f57b942d1d3f817c6
gitavhi <devkotaavishek208@gmail.com>
2024-07-18 08:05:13 +0000

    DOEN

---
+++ 01_basic/06_numAndmath.js
@@ -0,0 +1,28 @@
+const score = 100
+//console.log(score);
+const balance = new Number(100);
+////console.log(balance);
+//console.log(balance.toString());
+//console.log(balance.toFixed(2));
+
+const oneNumber = 123.8765
+//console.log(oneNumber.toPrecision(5));
+
+const hundreds = 100000000
+console.log(hundreds.toLocaleString('eng-IN'));
+
+
+
+//--------------------------MATHS------------------------//
+// console.log(Math);
+// console.log(Math.abs(-44));
+// console.log(Math.round(4.888));
+// console.log(Math.ceil(6.3));//top value
+// console.log(Math.floor(4.9));//lower value
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log((Math.floor)(Math.random()*10)+1);
+K
+
+
