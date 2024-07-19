a8f1a73fce4ba7a18792c983e609d8200e12b243
gitavhi <devkotaavishek208@gmail.com>
2024-07-19 10:15:24 +0000

    math and num

--- 01_basic/06_numAndmath.js
+++ 01_basic/06_numAndmath.js
@@ -23,6 +23,9 @@ console.log(hundreds.toLocaleString('eng-IN'));
 console.log(Math.random());
 console.log((Math.random()*10)+1);
 console.log((Math.floor)(Math.random()*10)+1);
-K
 
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min )+ 1 )+ min); 
 
